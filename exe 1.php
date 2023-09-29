7460a819e73e46c10bd24b3070ff9516f8e79ea4
Gui1304 <130668551+Gui1304@users.noreply.github.com>
2023-09-29 08:52:00 -0300

    commit brabo

---
+++ exe 1.php
@@ -0,0 +1,30 @@
+<?php
+
+// Declaração das variáveis
+$a = 5;
+$b = 2;
+$nome_1 = "Robson";
+$nome_2 = "Pedro";
+$c = 12;
+$d = 11;
+
+// Estrutura if
+if ($a > $b) {
+  echo "5 é maior que 2";
+} else {
+  echo "5 não é maior que 2";
+}
+
+if ($nome_1 != $nome_2) {
+  echo "Robson é diferente de Pedro";
+} else {
+  echo "Robson não é diferente de Pedro";
+}
+
+if ($c <= $d) {
+  echo "12 é menor ou igual a 11";
+} else {
+  echo "12 não é menor ou igual a 11";
+}
+
+?>
